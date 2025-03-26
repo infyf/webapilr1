b9ea4ba0f24139621ab1c1138b701074829c67ed
infyf <138444902+infyf@users.noreply.github.com>
2025-03-26 14:09:55 +0200

    Create Program.cs

---
+++ lab5/Program.cs
@@ -0,0 +1,27 @@
+using lab5.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<ApiClient>();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
+app.Run();
+

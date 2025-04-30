f980773fb49d7ad2765bc7c4352c622caecef82d
infyf <138444902+infyf@users.noreply.github.com>
2025-04-30 12:40:43 +0300

    Create Dish.cs

---
+++ lab6/Models/Dish.cs
@@ -0,0 +1,10 @@
+namespace lr9.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
